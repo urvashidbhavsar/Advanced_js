da0d9137ed3614de110c8dd13d50943bc12f22b9
Urvashi D. Bhavsar <109721868+urvashidbhavsar@users.noreply.github.com>
2024-07-09 16:30:33 +0530

    Add files via upload

---
+++ map/has.js
@@ -0,0 +1,7 @@
+var map = new Map();
+map.set(1, "angular.js");
+map.set(2, "vue.js");
+map.set(3, "react.js");
+
+console.log(map.has(3));
+console.log(map.has(4));
---
+++ map/keys.js
@@ -0,0 +1,11 @@
+var map = new Map();
+map.set(1, "angular.js");
+map.set(2, "vue.js");
+map.set(3, "react.js");
+
+let subj = map.keys();
+
+// using loop 
+for (let i = 0; i < map.size; i++) {
+    console.log(subj.next().value);
+}
