118b9e9c52f0cb1b7461d5adabd7b216637998c9
Urvashi D. Bhavsar <109721868+urvashidbhavsar@users.noreply.github.com>
2024-07-09 16:23:07 +0530

    Delete Advanced_js/functions directory

--- Advanced_js/functions/3_call.js
+++
@@ -1,9 +0,0 @@
-function emp(id, name) {
-    this.id = id;
-    this.name = name;
-}
-function employee(id, name) {
-    emp.call(this, id, name);
-}
-console.log(new employee(101, "Urvashi").id);
-console.log(new employee(101, "Urvashi").name);
--- Advanced_js/functions/4_bind.js
+++
@@ -1,9 +0,0 @@
-var thispage = {
-    name: "Current Page",
-    getName: function () {
-        return this.name
-    }
-}
-var newpages = thispage.getName;
-var allpage = newpages.bind(thispage);
-console.log(allpage());
